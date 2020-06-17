a3efe6e24d464fa89a7abf5ead607fc420956bbf
J Ferro <jonafi@gmail.com>
2020-06-17 14:49:48 -0500

    added view department function

--- employee-tracker.sql
+++ employee-tracker.sql
@@ -3,7 +3,7 @@ CREATE DATABASE employee_cms_db;
 USE employee_cms_db;
 
 CREATE TABLE department(
-  id INTEGER PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
 
@@ -16,10 +16,11 @@ VALUES
     (5, 'Engineering');
 
 CREATE TABLE role(
-  id INTEGER PRIMARY KEY,
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL(10),
-  department_id INTEGER
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 INSERT INTO role(id, title, salary, department_id)
@@ -27,29 +28,44 @@ VALUES
   	(1, 'Intern', 30000, 5),
     (2, 'Associate', 40000, 1),
     (3, 'Manager', 50000, 3),
-    (4, 'Director', 60000, 5),
-    (5, 'Intern,', 32000, 5),
-  	(6, 'Associate,', 42000, 3),
-    (7, 'Associate', 44000, 5),
-    (8, 'Manager', 55000, 2),
-    (9, 'Director', 66000, 1),
-    (10, 'Associate', 48000, 4);
+    (4, 'Director', 60000, 5);
 
 CREATE TABLE employee(
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INTEGER,
-  manager_id INTEGER
+  manager_id INTEGER, 
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee(id, first_name, last_name, role_id)
 VALUES
-	(101, 'Bill', 'Dentern', 1, 4),
-    (102, 'Susan', 'Anasc', 2, 9 ),
-    (103, 'Peter', 'Mando', 3, null),
-    (104, 'Steven', 'Spielberg', 5, null);
+	(101, 'Bill', 'Dentern', 1),
+    (102, 'Susan', 'Anasc' ,2),
+    (103, 'Peter', 'Mando', 3),
+    (104, 'Steven', 'Spielberg', 4);
+    
+UPDATE employee
+SET manager_id= 104
+WHERE id<104;
     
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
+
+SELECT employee.first_name, employee.last_name, role.title, department.name
+FROM ((employee
+INNER JOIN role ON employee.role_id = role.id)
+INNER JOIN department ON role.department_id = department.id)
+WHERE department.name = 'Engineering';
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -64,10 +64,11 @@ function selectAction() {
 
       switch (answer.actionSelection) {
         case "View All Employees":
-          displayEmployees();  // Done?
+          displayEmployees();
           break;
         case "View All Employees By Department":
           console.log("view all emp. by dept. function"); // Sort? Join?
+          viewByDepartment();
           break;
         case "View All Employees By Manager":
           console.log("view all emp. by manager function"); // Sort? Join?
@@ -131,6 +132,33 @@ function displayEmployees() {
   );
 }
 
+function viewByDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "departmentSelection",
+        type: "list",
+        message: "Select a department",
+        choices: ["Engineering", "Accounting", "Finance", "IT"]
+      }
+    ])
+    .then(function (answer) {
+      let queryText = `SELECT employee.first_name, employee.last_name, role.title, department.department_name
+  FROM ((employee INNER JOIN role ON employee.role_id = role.id)
+  INNER JOIN department ON role.department_id = department.id)
+  WHERE department.department_name = '${answer.departmentSelection}';`;
+
+      connection.query(queryText, function (err, data) {
+        if (err) throw err;
+        console.table("\nX Department".inverse, data);
+        if (data = "[]") console.log("No Matches\n".red)
+        selectAction();
+
+      });
+    }
+    );
+  }
+
 // function displayAll() {
 //   displayDepartments();
 //   displayRoles();
@@ -139,109 +167,109 @@ function displayEmployees() {
 
 
 function addEmployee() {
-  inquirer
-    .prompt([
-      {
-        name: "employeeFirstName",
-        type: "input",
-        message: "Enter Employee First Name",
-      },
-      {
-        name: "employeeLastName",
-        type: "input",
-        message: "Enter Employee Last Name",
+        inquirer
+          .prompt([
+            {
+              name: "employeeFirstName",
+              type: "input",
+              message: "Enter Employee First Name",
+            },
+            {
+              name: "employeeLastName",
+              type: "input",
+              message: "Enter Employee Last Name",
+            }
+          ])
+          .then(function (answer) {
+            connection.query(
+              "INSERT INTO employee SET ?",
+              {
+                first_name: answer.employeeFirstName,
+                last_name: answer.employeeLastName,
+                role_id: 5,
+                manager_id: null
+              },
+              function (err) {
+                if (err) throw err;
+                console.log("\n\tEmployee Added!\n".green);
+                selectAction();
+              }
+            );
+          });
       }
-    ])
-    .then(function (answer) {
-      connection.query(
-        "INSERT INTO employee SET ?",
-        {
-          first_name: answer.employeeFirstName,
-          last_name: answer.employeeLastName,
-          role_id: 5,
-          manager_id: null
-        },
-        function (err) {
-          if (err) throw err;
-          console.log("\n\tEmployee Added!\n".green);
-          selectAction();
-        }
-      );
-    });
-}
 
 function removeEmployee() {
-  connection.query("SELECT * FROM employee", function (err, data) {
-    if (err) throw err;
-    inquirer
-      .prompt(
-        {
-          name: "employeeSelected",
-          type: "rawlist",
-          choices: function () {
-            let choiceArray = [];
-            
-            for (let i = 0; i < data.length; i++) {
-              choiceArray.push(data[i].first_name + " " + data[i].last_name + " ID# " + data[i].id);
+        connection.query("SELECT * FROM employee", function (err, data) {
+          if (err) throw err;
+          inquirer
+            .prompt(
+              {
+                name: "employeeSelected",
+                type: "rawlist",
+                choices: function () {
+                  let choiceArray = [];
+
+                  for (let i = 0; i < data.length; i++) {
+                    choiceArray.push(data[i].first_name + " " + data[i].last_name + " ID# " + data[i].id);
+                  }
+                  return choiceArray;
+                },
+                message: "Select Employee to remove".red
+              },
+            )
+            .then(function (data) {
+              let idToDelete = data.employeeSelected.slice((data.employeeSelected.indexOf("#") + 2), data.employeeSelected.length)
+              //console.log("write del query that uses:" + idToDelete);
+              let deleteQuery = "DELETE FROM employee WHERE id=" + idToDelete;
+              connection.query(deleteQuery, function (err) {
+                if (err) {
+                  throw err;
+                }
+                console.log("Employee Removed".red);
+                selectAction();
+              }
+
+              )
+
             }
-            return choiceArray;
-          },
-          message: "Select Employee to remove".red
-        },
-      )
-      .then(function (data) {
-        let idToDelete = data.employeeSelected.slice((data.employeeSelected.indexOf("#") + 2), data.employeeSelected.length)
-        //console.log("write del query that uses:" + idToDelete);
-        let deleteQuery = "DELETE FROM employee WHERE id=" + idToDelete;
-        connection.query(deleteQuery, function (err){
-          if(err){
-            throw err;
-          }
-          console.log("Employee Removed".red);
-          selectAction();
+            )
         }
-
         )
-
       }
-      )
-  }
-  )
-}
 
 function addRole() {
-  connection.query(
-    "INSERT INTO role SET ?",
-    {
-      id: 11,
-      title: "Partner",
-      salary: 100000,
-      department_id: 4
-    },
-    function (err) {
-      if (err) throw err;
-      console.log("\n\tRole Added!\n".green);
-      selectAction();
-    }
-  );
-}
+        connection.query(
+          "INSERT INTO role SET ?",
+          {
+            id: 11,
+            title: "Partner",
+            salary: 100000,
+            department_id: 4
+          },
+          function (err) {
+            if (err) throw err;
+            console.log("\n\tRole Added!\n".green);
+            selectAction();
+          }
+        );
+      }
 
 function exitCheck() {
-  inquirer
-    .prompt({
-      name: "exitConfirm",
-      type: "confirm",
-      message: "Are you sure you want to exit?".yellow,
-    })
-    .then(function (answer) {
-      if (answer.exitConfirm == true) {
-        connection.end();
+        inquirer
+          .prompt({
+            name: "exitConfirm",
+            type: "confirm",
+            message: "Are you sure you want to exit?".yellow,
+          })
+          .then(function (answer) {
+            if (answer.exitConfirm == true) {
+              connection.end();
+            }
+            else {
+              selectAction();
+            }
+          });
       }
-      else {
-        selectAction();
-      }
-    });
-}
 
 
 // function bidAuction() {
